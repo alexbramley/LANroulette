c8e4f43c0ed71ac21910eeeb1d247351837c8635
alexbramley <epicmrbramley@gmail.com>
2025-02-18 15:33:20 +0000

    added chatlog

--- app.py
+++ app.py
@@ -1,14 +1,42 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 import socket
 
 PORT_NUMBER = 42069
 
+users = {} # this dict contains {ip: user}
+
+class User:
+    def __init__(self, ip_address, nickname):
+        self.ip_address = ip_address
+        self.nickname = nickname
+
+chatlog = []
+
 app = Flask(__name__)
+
 
 @app.route('/', methods=['POST','GET'])
 def index():
     ip = request.remote_addr
-    return render_template('index.html', ip=ip)
+
+    if request.method == 'GET':
+        if users.__contains__(ip):
+            message = "welcome back"
+        else:
+            message = "hello new person"
+            users.update({ip: User(ip, "<PLACEHOLDER>")})
+
+        return render_template('index.html', ip=ip, message=message)
+    
+    else:
+        nickname = request.form['getnickname']
+        users[ip].nickname = nickname
+        chatlog.append(f"{nickname} is here.")
+        return redirect('/GAMING')
+
+@app.route('/GAMING', methods=['POST','GET'])
+def gaming():
+    return chatlog
 
 hostname=socket.gethostname()
 hostipaddress = socket.gethostbyname(hostname)
