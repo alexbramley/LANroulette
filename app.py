41da1e73393c6fbd6a8d0cd0b41be428e5738f89
alexbramley <epicmrbramley@gmail.com>
2025-02-18 15:10:40 +0000

    it gets ur ip

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+import socket
+
+PORT_NUMBER = 42069
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
-    return render_template('index.html')
+    ip = request.remote_addr
+    return render_template('index.html', ip=ip)
+
+hostname=socket.gethostname()
+hostipaddress = socket.gethostbyname(hostname)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=hostipaddress, port=PORT_NUMBER, debug=True)
